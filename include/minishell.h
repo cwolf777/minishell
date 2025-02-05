8fc387b7c4bae3b2bef78742c2adafa4cc0f9522
Philipp Hofmann <phofmann200@gmail.com>
2025-02-05 13:42:42 +0100

    update makefile and header

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: phhofman <phhofman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/31 13:06:36 by phhofman          #+#    #+#             */
-/*   Updated: 2025/01/31 17:31:18 by phhofman         ###   ########.fr       */
+/*   Updated: 2025/02/05 13:42:18 by phhofman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,5 +27,8 @@ char	*get_cmd_path(char *cmd, char *envp[]);
 char	**get_paths(char *envp[]);
 void	free_split(char **split);
 void	handle_error(char *error_msg, int exit_status);
+
+// parse
+char	*read_prompt();
 
 #endif
